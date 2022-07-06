4f1dd8359a42a11695cec53546d89508503aed3f
Ali Ali <aliali_266@yahoo.com>
2022-07-07 00:49:23 +0400

    changed firstname to firstName in USER

--- backend/db/seeders/20220629200814-demo-user.js
+++ backend/db/seeders/20220629200814-demo-user.js
@@ -6,36 +6,36 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [
       {
         email: 'demo@user.io',
-        firstname: 'John',
-        lastname: 'Smith',
+        firstName: 'John',
+        lastName: 'Smith',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
-        firstname: 'Jane',
-        lastname: 'Smith',
+        firstName: 'Jane',
+        lastName: 'Smith',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
-        firstname: 'Elton',
-        lastname: 'John',
+        firstName: 'Elton',
+        lastName: 'John',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
       },
       {
         email: 'user3@user.io',
-        firstname: 'Chris',
-        lastname: 'Rock',
+        firstName: 'Chris',
+        lastName: 'Rock',
         username: 'FakeUser3',
         hashedPassword: bcrypt.hashSync('password4')
       },
       {
         email: 'user4@user.io',
-        firstname: 'Morgan',
-        lastname: 'Freeman',
+        firstName: 'Morgan',
+        lastName: 'Freeman',
         username: 'FakeUser4',
         hashedPassword: bcrypt.hashSync('password5')
       },
