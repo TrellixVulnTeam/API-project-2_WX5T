1c40f9bf97b3a16a8e766e78204f6558af19cdf8
Ali Ali <aliali_266@yahoo.com>
2022-07-08 02:42:16 +0400

    fix deployment issue

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { handleValidationErrors } = require('../../utils/validation');
 const { Sequelize, Model, DataTypes } = require("sequelize");
-const sequelize = new Sequelize("sqlite::memory:");
+// const sequelize = new Sequelize("sqlite::memory:");
 const { requireAuth } = require("../../utils/auth");
 const { check } = require("express-validator");
-
+const {sequelize} = require("../../db/models")
 const { Spots, Review, User, Image } = require("../../db/models");
 // const spots = require('../../db/models/spots')
 const router = express.Router();
