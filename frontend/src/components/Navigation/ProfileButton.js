713e2fc3b7bf7a230f6bc86ab95599979dc4af93
Ali Ali <aliali_266@yahoo.com>
2022-07-29 00:26:20 +0400

    Navigation

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import * as sessionActions from "../../store/session";
+import "./Navigation.css";
+
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+    document.addEventListener("click", closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
+
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
+  return (
+    <>
+      <div className="button">
+        <button className="navBar" onClick={openMenu}>
+          <i className="fas fa-bars nav_bars_icon"></i>
+          <i className="fas fa-user-circle user_icon"></i>
+        </button>
+        {showMenu && (
+          <div id="menu">
+            <Link to="/spots/create" id="dropdown1">
+              Create a Spot
+            </Link>
+            <Link to="/users/current/spots">My Spots</Link>
+            <div onClick={logout} id="dropdown2">
+              Log out
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default ProfileButton;
