d009f892822fcd779527d3722e1e762c51c9ad5e
Ali Ali <aliali_266@yahoo.com>
2022-07-29 00:42:13 +0400

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,52 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import SignupFormPage from "./components/SignupFormPage";
+import LoginFormModal from "./components/LoginFormModal";
+import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
+import SpotDetail from "./components/SpotDetail";
+import SpotsPage from "./components/Spots";
+import NewSpotForm from "./components/SpotsForm";
+import EditSpot from "./components/SpotEdit"
+import UserSpots from "./components/UserSpots";
+
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route exact path="/">
+            <SpotsPage />
+          </Route>
+          <Route exact path="/signup">
+            <SignupFormPage />
+          </Route>
+          <Route exact path="/login">
+            <LoginFormModal />
+          </Route>
+          <Route exact path="/spots/create">
+            <NewSpotForm />
+          </Route>
+          <Route exact path="/spots/:spotId/edit">
+            <EditSpot />
+          </Route>
+          <Route exact path="/spots/:spotId">
+            <SpotDetail />
+          </Route>
+          <Route exact path="/users/current/spots">
+            <UserSpots />
+          </Route>
+        </Switch>
+      )}
+    </>
   );
 }
 
